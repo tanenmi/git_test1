56b17ccf6f17fd49f4e3856c52b6162957ff22f7
tanenmi <88675170+tanenmi@users.noreply.github.com>
2025-02-07 02:51:52 +0900

    Revert "Branch1"

--- git_restart/九九.py
+++
@@ -1,7 +0,0 @@
-digits = range(1, 10)
-print('|  |', end = '')
-print('|'.join('%2d' % i for i in digits))
-print('+' + '--+' * 10)
-for a in digits:
-    print('|%2d|' % a, end = '')
-    print(''.join('%2d|' % (a * b) for b in digits))
